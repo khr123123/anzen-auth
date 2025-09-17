72d7c0d227fee2d7ba840472a8d73e97bae8ff17
3031208 <21@qwe>
2025-09-17 09:35:57 +0900

    feat: 初始化 Anzen Auth 项目
    - 创建 Spring Boot 应用程序主类和测试类
    - 添加 Maven 构建配置文件 pom.xml
    - 设置 .gitattributes 和 .gitignore 文件
    - 包含 Maven Wrapper 配置和脚本文件

---
+++ src/main/java/org/khr/anzenauth/AnzenAuthApplication.java
@@ -0,0 +1,13 @@
+package org.khr.anzenauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnzenAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnzenAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/khr/anzenauth/AnzenAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package org.khr.anzenauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnzenAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
