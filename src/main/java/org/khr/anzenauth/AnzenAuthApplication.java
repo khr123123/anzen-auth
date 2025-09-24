d656d9860783e5d31c438e858ce0e55f4cbae242
3031208 <21@qwe>
2025-09-24 16:27:10 +0900

    修正

--- src/main/java/org/khr/anzenauth/AnzenAuthApplication.java
+++ src/main/java/org/khr/anzenauth/AnzenAuthApplication.java
@@ -5,9 +5,11 @@ import org.khr.anzenauth.manager.async.AsyncManager;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @MapperScan("org.khr.anzenauth.mapper")
+@EnableAspectJAutoProxy(exposeProxy = true)
 public class AnzenAuthApplication {
 
     public static void main(String[] args) {
